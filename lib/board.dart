f59db5c557cfcfd1075588b70c32ab7a68cfa8ab
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-10 17:29:35 +0000

    Add Board.rows and cells generation

--- lib/board.dart
+++ lib/board.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
 import 'cell.dart';
+import 'cell_group.dart';
 
 /// The board that a puzzle takes place within.
 class Board extends StatelessWidget {
-  Board({super.key}) : cells = [Cell()];
+  Board({super.key})
+      : rows = List<BoardRow>.from(
+          List<int>.generate(dimension, (int index) => index).map(
+            (int rowIndex) => BoardRow(index: rowIndex),
+          ),
+        ) {
+    cells = () {
+      List<Cell> cells = [];
+
+      for (BoardRow row in rows) {
+        cells.addAll(row.cells);
+      }
+
+      return cells;
+    }();
+  }
 
   static const int dimension = 9;
 
-  final List<Cell> cells;
+  late final List<Cell> cells;
+  final List<BoardRow> rows;
 
   @override
   Widget build(BuildContext context) {
