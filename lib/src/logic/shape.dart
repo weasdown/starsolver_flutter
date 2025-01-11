bdd72800f084a0370a0fa19b021eb6e651dbf263
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-01-11 11:26:51 +0000

    Add generateShapeColours() placeholder

--- lib/src/logic/shape.dart
+++ lib/src/logic/shape.dart
@@ -1,6 +1,8 @@
-import 'package:color/color.dart';
+import 'dart:ui';
 
-import 'board.dart';
+// import 'package:color/color.dart' as c;
+import 'package:flutter/material.dart';
+
 import 'cell.dart';
 
 /// One of the nine shapes within a [Board], that contains several [Cell]s.
@@ -14,4 +16,45 @@ class Shape {
   /// Gets whether the [Shape] is a special shape, where a star or dot can be instantly located.
   bool get isSpecial =>
       throw UnimplementedError('Shape.isSpecial is not yet implemented.');
+}
+
+/// Gets a list of nine [Color]s for use in [Shape]s.
+///
+/// The [Color]s are spaced regularly around the colour wheel to have good
+/// contrast against each other.
+List<Color> generateShapeColours() {
+  throw UnimplementedError('generateShapeColours is not yet implemented.');
+
+  // double startH = 0;
+  // double startS = 0;
+  // double startL = 0;
+  //
+  // // Step round the hue circle by 40 degrees per colour, to get 9 regularly spaced colours.
+  // final int delta = 40;
+  //
+  // List<c.Color> hslColors = List<c.Color>.generate(
+  //   9,
+  //   (int index) => c.Color.hsl(
+  //     startH + index * delta,
+  //     startS,
+  //     startL,
+  //   ),
+  // );
+  //
+  // return List<Color>.from(
+  //   hslColors.map(
+  //       // FIXME fix colour output
+  //       (c.Color hsl) => Color.from(alpha: 255, red: 0, green: 0, blue: 255)),
+  // );
+  //
+  // // TODO remove dead colour code
+  // // return List.generate(9, (int index) => const Color(0xFF42A5F5)
+  // //     // Color.hsl(startH + index * delta, startS, startL)
+  // //     //     Color.from(
+  // //     //   alpha: 0,
+  // //     //   red: 0,
+  // //     //   green: 0,
+  // //     //   blue: 255,
+  // //     // ),
+  // //     );
 }
